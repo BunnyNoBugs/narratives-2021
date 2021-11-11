63822ae0d6e4626086cedc60a75a67074138c01a
NoBugsHere <eugene200020@gmail.com>
2021-11-11 15:47:45 +0300

    add edu dfs

--- rst_to_df.py
+++ rst_to_df.py
@@ -16,10 +16,12 @@ def _parse_syntax(sents: Iterable[str]):
 
 
 def _preprocess_segment(segment: Dict):
-    if segment['segment'].startswith(('О:', 'М:')):
+    if segment['segment'].startswith(('О:', 'М:', 'М\t', 'Э\t', 'В\t')):
         segment['segment'] = segment['segment'][2:]
         segment['is_prompt'] = 1
     else:
+        if segment['segment'].startswith(('Р:', 'Р\t', 'О\t')):
+            segment['segment'] = segment['segment'][2:]
         segment['is_prompt'] = 0
     return segment
 
@@ -38,11 +40,6 @@ def _rst_to_df(rst_soup: BeautifulSoup):
 
 
 def _rst_df_to_tokens_df(rst_df):
-    parsed_segments = []
-    # for segment in rst_df['segment']:
-    #     print(segment)
-    #     parsed_segments.append(_parse_syntax([segment])[0])
-    # parsed_segments = [_parse_syntax([s])[0] for s in rst_df['segment']]
     parsed_segments = _parse_syntax(rst_df['segment'])
     tokens = []
     for parsed, segment in zip(parsed_segments, rst_df.iterrows()):
@@ -51,6 +48,26 @@ def _rst_df_to_tokens_df(rst_df):
             token['segment_id'] = segment[1]['id']
             token['parent'] = segment[1]['parent']
             token['relname'] = segment[1]['relname']
+            token['is_prompt'] = segment[1]['is_prompt']
+            tokens.append(token)
+    tokens_df = pd.DataFrame(tokens)
+    return tokens_df
+
+
+def _txt_to_edu_df(text: str):
+    segments = [{'segment': x} for x in text.split('\n') if x and not x.isspace()]
+    segments = [_preprocess_segment(s) for s in segments]
+    edu_df = pd.DataFrame(segments)
+    edu_df['segment_id'] = pd.Series(range(1, len(edu_df) + 1))
+    return edu_df
+
+
+def _edu_df_to_tokens_df(edu_df):
+    parsed_segments = _parse_syntax(edu_df['segment'])
+    tokens = []
+    for parsed, segment in zip(parsed_segments, edu_df.iterrows()):
+        for token in parsed:
+            token['segment_id'] = segment[1]['segment_id']
             token['is_prompt'] = segment[1]['is_prompt']
             tokens.append(token)
     tokens_df = pd.DataFrame(tokens)
@@ -67,6 +84,14 @@ def convert_rst_to_tokens_df(load_path, save_path):
     tokens_df.to_csv(save_path, index=False)
 
 
+def convert_edu_to_tokens_df(load_path, save_path):
+    with open(load_path) as f:
+        text = f.read()
+    edu_df = _txt_to_edu_df(text)
+    tokens_df = _edu_df_to_tokens_df(edu_df)
+    tokens_df.to_csv(save_path, index=False)
+
+
 def unite_dfs(folder_path: str):
     dfs = []
     for filename in os.listdir(folder_path):
@@ -79,12 +104,23 @@ def unite_dfs(folder_path: str):
 
 
 def main():
-    rst_texts_path = 'rst_texts'
-    for filename in os.listdir(rst_texts_path):
-        file_path = os.path.join(rst_texts_path, filename)
-        if os.path.isfile(file_path):
-            convert_rst_to_tokens_df(file_path, os.path.join('rst_df', os.path.splitext(filename)[0] + '.csv'))
-    unite_dfs('rst_df').to_csv('bilingual.csv', index=False)
+    # RST to tokens
+    # rst_texts_path = 'rst_texts'
+    # for filename in os.listdir(rst_texts_path):
+    #     file_path = os.path.join(rst_texts_path, filename)
+    #     if os.path.isfile(file_path):
+    #         convert_rst_to_tokens_df(file_path, os.path.join('rst_df', os.path.splitext(filename)[0] + '.csv'))
+
+    # EDU to tokens
+    # for group in ['adult', 'bilingual', 'monolingual']:
+    #     group_folder_path = os.path.join('EDU markup', group)
+    #     for filename in os.listdir(group_folder_path):
+    #         file_path = os.path.join(group_folder_path, filename)
+    #         convert_edu_to_tokens_df(file_path, os.path.join('edu_df', group, os.path.splitext(filename)[0] + '.csv'))
+
+    for group in ['adult', 'bilingual', 'monolingual']:
+        group_folder_path = os.path.join('edu_df', group)
+        unite_dfs(group_folder_path).to_csv(f'{group}.csv', index=False)
 
 
 if __name__ == '__main__':
